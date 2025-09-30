19493962e6911866649bd7980688d8604b069268
manasha258 <manasha258@gmail.com>
2025-09-30 12:40:19 +0530

    Biodata

---
+++ src/main/java/basics/Biodata.java
@@ -0,0 +1,13 @@
+package basics;
+
+
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println(" Name:Manasha.k");
+        System.out.println(" College:vinayaka Mission College");
+        System.out.println("Age:21");
+        System.out.println("Height:156");
+        System.out.println("Weight:49");
+        System.out.println("helloo");
+    }
+}
