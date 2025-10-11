3fb376efedcc5b39c171cdb32270d55513d7a08c
manasha258 <manasha258@gmail.com>
2025-10-11 10:48:19 +0530

    datatypes and casting

---
+++ src/main/java/basics/datatypes.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class datatypes {
+    public static void main(String[] args) {
+        int marks=10;
+        float lab=28.3f;
+        double Model=95.82;
+        long Registernumber=611223104090l ;
+
+        String college="VMRF";
+        System.out.println("My marks is:"+marks);
+        System.out.println("My lab:"+lab);
+        System.out.println("My Model is:"+Model);
+        System.out.println("My Registernumber is:"+Registernumber);
+        System.out.println("My college name is:"+college);
+
+    }
+}
---
+++ src/main/java/basics/typecasting.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class typecasting {
+    public static void main(String[] args) {
+
+        char letter = 'M';
+        int number = 2;
+        float value = 2.8f;
+        double assign = 57.99;
+        int change=(int)assign;
+        float alter=(float)number;
+        char alternate=(char)assign;
+        double name=(double)number;
+        System.out.println("my number is:"+change);
+        System.out.println("My value is:"+alter);
+        System.out.println("My letter is:"+alternate);
+        System.out.println("My int value is:"+name);
+
+
+
+    }
+}
